3a8639ecf773c7cb75b6ae17f45f2be7246d99ab
yashrshinde <yashrshinde03@gmail.com>
2022-10-13 21:13:11 +0530

    Python Leet Code Solution

---
+++ python/Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,19 @@
+class Solution(object):
+   def lengthOfLongestSubstring(self, s):
+      i =0
+      j = 0
+      d={}
+      ans = 0
+      while j < len(s):
+         if s[j] not in d or i>d[s[j]]:
+            ans = max(ans,(j-i+1))
+            d[s[j]] = j
+         else:
+            i = d[s[j]]+1
+            ans = max(ans,(j-i+1))
+            j-=1
+         #print(ans)
+         j+=1
+      return ans
+ob1 = Solution()
+print(ob1.lengthOfLongestSubstring("ABCABCBB"))
